e2fa7dedd8af2d951dc87d07e03dfee388170c12
AJ Larson <aj@devmounta.in>
2020-11-21 16:20:31 -0700

    fixing a typo, updating comments

--- src/__tests__/feedTest_individualTests.ts
+++ src/__tests__/feedTest_individualTests.ts
@@ -1,6 +1,8 @@
 import { Animal } from "../animal";
 import { getAnimal } from "../zoo";
 
+// now we have an array of even more detailed objects, the liked foods are even
+// directly associated with the animal that should eat them
 let data = [
   { animal: getAnimal("Alex"), foods: ["meat"] },
   { animal: getAnimal("Marty"), foods: ["grass", "leaves", "shrubs", "bark"] },
@@ -8,11 +10,17 @@ let data = [
   { animal: getAnimal("Gloria"), foods: ["grass", "reeds", "shoots"] },
 ];
 
+// we left this out, because no animal likes them, we don't need to copy it to
+// each animal
 const badFoods = ["shrimp", "potatoes", "pizza", "icecream"];
 
 describe("feeding animals", () => {
+  // first, we iterate across each animal
   data.forEach((record) => {
+    // additional describe blocks just break down your testing more - giving
+    // more context to your test output.
     describe(`Feeding ${record.animal.name} the ${record.animal.type}`, () => {
+      // now we'll iterate through the liked foods, creating a new test for each
       record.foods.forEach((food) => {
         test(`They should like ${food}`, () => {
           expect(record.animal.feed(food)).toContain(`likes ${food}!`);
@@ -21,6 +29,7 @@ describe("feeding animals", () => {
           );
         });
       });
+      // and do the same for the unliked foods
       badFoods.forEach((food) => {
         test(`They should not like ${food}`, () => {
           expect(record.animal.feed(food)).not.toContain(`likes ${food}!`);
@@ -30,14 +39,4 @@ describe("feeding animals", () => {
     });
   });
 });
-
-let list = [
-  { name: "one", value: 1 },
-  { name: "two", value: 2 },
-  { name: "three", value: 3 },
-];
-list.forEach((item) => {
-  test(`We're looking for ${item.name}`, () => {
-    expect(item.value).toBeDefined();
-  });
-});
+// that is it - way cleaner, and easier to maintain, if you know how to do it.
